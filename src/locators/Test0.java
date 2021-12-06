2bdcd2448c1179ebdcab01b1d05e7e3b3af7e2e4
QSP <QSP@QSP>
2021-12-06 18:00:49 +0530

    HTML

---
+++ src/locators/Test0.java
@@ -0,0 +1,21 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+WebDriver driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://demo.actitime.com/login.do");
+
+driver.findElement(By.linkText("username")).sendKeys("rytr767trytwr76");
+driver.findElement(By.name("pwd")).sendKeys("76467yusyugfsg");
+
+
+
+	}
+
+}
---
+++ src/locators/Test1.java
@@ -0,0 +1,20 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("4546q6547df");
+        driver.findElement(By.name("pass")).sendKeys("gfgfjgjfj");
+	driver.findElement(By.name("login")).click();
+	
+	}
+
+}
