e0ab4b3e4c9889e75ffce4def2530bdd1699c4ab
QSP <QSP@QSP>
2021-12-17 18:01:37 +0530

    scrollbar

---
+++ src/actions/Test1.java
@@ -0,0 +1,29 @@
+package actions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Test1 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoapp.skillrary.com/product.php?product=selenium-training");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+		WebElement button = driver.findElement(By.id("add"));
+      
+		Thread.sleep(5000);
+		Actions a=new Actions(driver);
+      a.doubleClick(button).perform();
+      
+		
+	}
+
+}
