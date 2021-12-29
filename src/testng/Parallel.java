78de3c1dec19d150ad50ee485fa80c0047a617d4
QSP <QSP@QSP>
2021-12-29 18:34:00 +0530

    Testng

--- src/testng/Parallel.java
+++ src/testng/Parallel.java
@@ -7,10 +7,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Parallel {
 	public WebDriver driver;
+	
+	@Parameters({"browserName"})
 	@Test
 	public void login(String browser) {
 		if(browser.equals("chrome")) {
