ad411b71af82df9182f3854abc148b71bb698cb0
QSP <QSP@QSP>
2022-01-08 16:37:13 +0530

    Hi

---
+++ src/demo/Test.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+//it is used to print the word hello java
+/*System.out.println("hello java");
+
+System.out.println("hello python");
+System.out.println("hello Api");*/
+System.out.println("hello Appium");
+
+	}
+
+}
